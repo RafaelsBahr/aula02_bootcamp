4d3ea9f226aeffbe04058c34d6cf95588d638035
RafaelsBahr <rafaelsedrezbahr@gmail.com>
2026-05-27 10:33:00 -0300

    Feat: Exercicios 16 e 17!

---
+++ 4-exercicio_bool.py
@@ -0,0 +1,17 @@
+# #### Booleanos (`bool`)
+
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+# val1 = input("Digite a primeira expressão (True ou False): ")
+# val2 = input("Digite a segunda expressão (True ou False): ")
+# resultado = val1 and val2
+# print(f"O resultado da operação AND entre {val1} e {val2} é: {resultado}")
+
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+# val1 = input("Digite a primeira expressão (True ou False): ")
+# val2 = input("Digite a segunda expressão (True ou False): ")
+# resultado = val1 or val2
+# print(f"O resultado da operação OR entre {val1} e {val2} é: {resultado}")
+
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
