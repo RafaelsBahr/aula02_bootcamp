9b51938a3aedc660877bf2ac7a28b690028cfd3a
RafaelsBahr <rafaelsedrezbahr@gmail.com>
2026-05-27 14:31:53 -0300

    Fix: No exercício 18 descobri que fiz o 16 e 17 errados. Aprendi, voltei e corrigi!

--- 4-exercicio_bool.py
+++ 4-exercicio_bool.py
@@ -1,17 +1,43 @@
 # #### Booleanos (`bool`)
 
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
-# val1 = input("Digite a primeira expressão (True ou False): ")
-# val2 = input("Digite a segunda expressão (True ou False): ")
+# Primeira tentativa
+    # val1 = input("Digite a primeira expressão (True ou False): ")
+    # val2 = input("Digite a segunda expressão (True ou False): ")
+    # resultado = val1 and val2
+    # print(f"O resultado da operação AND entre {val1} e {val2} é: {resultado}")
+# Segunda tentativa
+# val1 = input("Digite a primeira expressão: ").capitalize() == "True"
+# val2 = input("Digite a segunda expressão: ").capitalize() == "True"
 # resultado = val1 and val2
 # print(f"O resultado da operação AND entre {val1} e {val2} é: {resultado}")
 
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
-# val1 = input("Digite a primeira expressão (True ou False): ")
-# val2 = input("Digite a segunda expressão (True ou False): ")
+# Primeira tentativa
+    # val1 = input("Digite a primeira expressão (True ou False): ")
+    # val2 = input("Digite a segunda expressão (True ou False): ")
+    # resultado = val1 or val2
+    # print(f"O resultado da operação OR entre {val1} e {val2} é: {resultado}")
+# Segunda tentativa
+# val1 = input("Digite a primeira expressão: ").capitalize() == "True"
+# val2 = input("Digite a segunda expressão: ").capitalize() == "True"
 # resultado = val1 or val2
 # print(f"O resultado da operação OR entre {val1} e {val2} é: {resultado}")
 
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+# Esse exercício eu travei, pedi ajuda para IA. Mas ao invés de só pedir resposta fui perguntando as coisas para entender e agora entendi!
+# Antes eu tava achando que quando escrevia True ou False era boolean, mas era string convertido para booleano completamente errado.
+# String vazia = false > String com algum texto = True
+# Também conheci o comando .capitalize()
+# entrada = input("Digite a expressão (True ou False): ").capitalize()
+# val1 = (entrada == "True")
+# inverso = not val1
+# print(f"O valor original é {val1}, então o seu inverso é {inverso}")
+
+# entrada = input("Digite a expressão (True ou False): ").capitalize() =="True"
+# inverso = not entrada
+# print(f"O valor original é {entrada}, então o seu inverso é {inverso}")
+
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
 # 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
