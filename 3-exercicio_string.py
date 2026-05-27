a816d8b17e54c60071271451e4e14278f78fee83
RafaelsBahr <rafaelsedrezbahr@gmail.com>
2026-05-27 09:16:16 -0300

    Feat: Exercício 12 concluído!

--- 3-exercicio_string.py
+++ 3-exercicio_string.py
@@ -1,12 +1,15 @@
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
-print("Converter para maiúsculas")
-texto1 = str(input("Seu texto: "))
-print(texto1.upper())
-
+# print("Converter para maiúsculas")
+# texto1 = str(input("Seu texto: "))
+# print(texto1.upper())
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+print("Converter seu nome para minusculas!")
+texto1 = str(input("Seu Nome: "))
+print(texto1.lower())
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
