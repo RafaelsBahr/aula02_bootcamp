0d2460fd8aa0ffa9b241d9441bbb388412d038dc
RafaelsBahr <rafaelsedrezbahr@gmail.com>
2026-05-27 09:24:18 -0300

    Feat: Exercícios até 15 feitos!

--- 3-exercicio_string.py
+++ 3-exercicio_string.py
@@ -6,10 +6,23 @@
 # print(texto1.upper())
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
-print("Converter seu nome para minusculas!")
-texto1 = str(input("Seu Nome: "))
-print(texto1.lower())
+# print("Converter seu nome para minusculas!")
+# texto1 = str(input("Seu Nome: "))
+# print(texto1.lower())
 
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# print("Frase sem espaços!")
+# texto1 = str(input("Sua frase: "))
+# print(texto1.strip())
+
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# print("Separar datas!")
+# texto1 = str(input("Sua data (no formato dd/mm/yyyy): "))
+# print(texto1.split("/"))
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+print("Concatenar texto do usuário")
+texto1 = str(input("Seu primeiro texto: "))
+texto2 = str(input("Seu segundo texto: "))
+texto3 = str(input("Seu terceiro texto: "))
+print(texto1 + " " + texto2 + " " + texto3)
